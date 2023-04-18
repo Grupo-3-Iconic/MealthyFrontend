840ad8f6bf0623387f92b0199525d946cd0ee7d5
PolVega <u20201b333@upc.edu.pe>
2023-04-18 18:27:07 -0500

    Fixing some problems about project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')

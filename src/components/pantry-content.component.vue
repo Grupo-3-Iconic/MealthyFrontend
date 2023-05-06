f10934e0845eb14bd8f064c6c58318263959495f
ShaylaChoque <89035883+ShaylaChoque@users.noreply.github.com>
2023-05-06 03:47:41 -0500

    pantry-table-creation

--- src/components/pantry-content.component.vue
+++ src/components/pantry-content.component.vue
@@ -9,6 +9,28 @@
       <input id="item-unit" type="text" v-model="newItemUnit" required>
       <i class="pi pi-plus add-item-button" @click="onAddItem"></i>
     </form>
+    <table class="pantry-table">
+      <thead>
+        <tr>
+          <th>Insumo</th>
+          <th>Cantidad</th>
+          <th>Unidad</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index" class="pantry-item">
+          <td>{{ item.name }}</td>
+          <td>{{ item.quantity }}</td>
+          <td>
+            <input type="text" v-model="item.unit">
+          </td>
+          <td>
+            <i class="pi pi-trash delete-item-button" @click="onRemoveItem(index)"></i>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 

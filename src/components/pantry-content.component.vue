f014f2c538126b973b7bded957e939b9408ada9e
ShaylaChoque <89035883+ShaylaChoque@users.noreply.github.com>
2023-05-06 03:47:11 -0500

    pantry-form-creation

--- src/components/pantry-content.component.vue
+++ src/components/pantry-content.component.vue
@@ -1,4 +1,15 @@
 <template>
+  <div class="pantry-content">
+    <form @submit.prevent="onAddItem">
+      <label for="item-name"> Insumo </label>
+      <input id="item-name" type="text" v-model="newItemName" required>
+      <label for="item-quantity"> Cantidad </label>
+      <input id="item-quantity" type="number" v-model="newItemQuantity" required>
+      <label for="item-unit"> Unidad </label>
+      <input id="item-unit" type="text" v-model="newItemUnit" required>
+      <i class="pi pi-plus add-item-button" @click="onAddItem"></i>
+    </form>
+  </div>
 </template>
 
 <script>

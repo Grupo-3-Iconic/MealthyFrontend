fff9b51bc7bbe837f9ef0a866d095e9515910b23
Julio Asillo Mendoza <u20201b482@upc.edu.pe>
2023-05-24 20:37:43 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-
+  <side-menu/>
+  <search-store/>
 </template>
 
 <script setup>
 import SideMenu from "./components/side-menu.component.vue";
-import SideBar from "./components/side-menu-bar/side-bar.component.vue";
+import SearchStore from "./components/search-store.component.vue";
 
 </script>
 <style scoped>

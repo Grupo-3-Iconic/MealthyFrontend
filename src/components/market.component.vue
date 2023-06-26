74c153f8549297dee46eee9a997a2178ec8c1b58
Julio Asillo Mendoza <u20201b482@upc.edu.pe>
2023-06-26 00:21:51 -0500

    Update market.component.vue

--- src/components/market.component.vue
+++ src/components/market.component.vue
@@ -1,107 +1,116 @@
 <template>
-    <ToolbarComponent/>
-    <div>
-        <div class="markets-toolbar">
-            <pv-toolbar style="max-height: 80px;">
-                <template #start>
-                    <h1>Markets</h1>
-                </template>
-                <template #end>
-          <span class="p-input-icon-left">
-            <i class="pi pi-search" />
-            <pv-input-text v-model="filters['global'].value" placeholder="Buscar" @input="search" />
-          </span>
-                </template>
-            </pv-toolbar>
-            <pv-divider />
-        </div>
-        <div class="row">
-            <div v-for="market in filteredMarkets" :key="market.id" class="col">
-                <pv-card>
-                    <template #header>
-                        <img :src="market.photo" alt="Market Photo" class="market-image">
-                    </template>
-                    <template #title>{{ market.storeName }}</template>
-                    <template #content>
-                        <p>{{ market.description }}</p>
-                        <pv-button @click="viewMarket(market.id)">Ver más</pv-button>
-                    </template>
-                </pv-card>
-            </div>
-        </div>
+  <ToolbarComponent/>
+  <div>
+    <div class="markets-toolbar">
+      <pv-toolbar style="max-height: 80px;">
+        <template #start>
+          <h1>Markets</h1>
+        </template>
+        <template #end>
+                <span class="p-input-icon-left">
+                  <i class="pi pi-search" />
+                  <pv-input-text v-model="productName" placeholder="Buscar" @input="searchStoreId" />
+                </span>
+        </template>
+      </pv-toolbar>
+      <pv-divider />
     </div>
+    <div class="row">
+      <div v-for="market in markets" :key="market.id" class="col">
+        <pv-card>
+          <template #header>
+            <img :src="market.photo" alt="Market Photo" class="market-image">
+          </template>
+          <template #title>{{ market.storeName }}</template>
+          <template #content>
+            <p>{{ market.description }}</p>
+            <pv-button @click="viewMarket(market.id)">Ver más</pv-button>
+          </template>
+        </pv-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import ToolbarComponent from "@/components/toolbar.component.vue";
 import {MarketApiService} from "@/services/market-api.service";
+import {ProductApiService} from "@/services/product-api.service";
 
 export default {
-    name: "market.component.vue",
-    components: {ToolbarComponent},
+  name: "market.component.vue",
+  components: {ToolbarComponent},
 
-    data() {
-        return {
-            markets: [],
-            filteredMarkets: [],
-            marketService: new MarketApiService(),
-            filters: {
-                global: {
-                    value: null,
-                }
+  data() {
+    return {
+      productName: '',
+      markets: []
+    };
+  },
+  created() {
+    this.productApiService = new ProductApiService();
+    this.marketApiService = new MarketApiService();
+  },
+  methods: {
+    searchStoreId(){
+      this.productApiService.findByName(this.productName)
+          .then(response => {
+            const products = response.data;
+            const product = products.find(product => product.name === this.productName);
+
+            if(product){
+              const storeId = product.storeId;
+              this.searchMarkets(storeId);
+            }else {
+              this.markets=[];
             }
-        };
+          })
+          .catch(error=>{
+            console.error(error);
+            this.markets=[];
+          });
     },
-    created() {
-        this.getMarkets();
+    searchMarkets(storeId){
+      this.marketApiService.getAll()
+          .then(response=>{
+            const markets = response.data;
+            this.markets = markets.filter(market=>market.id === storeId);
+          })
+          .catch(error=>{
+            console.error(error);
+            this.markets=[];
+          });
     },
-    methods: {
-        getMarkets() {
-            this.marketService.getAll()
-                .then(response => {
-                    this.markets = response.data;
-                    this.filteredMarkets = response.data;
-                })
-                .catch(error => {
-                    console.error('Error fetching markets:', error);
-                });
-        },
-        search() {
-            const searchValue = this.filters['global'].value;
-            this.filteredMarkets = searchValue
-                ? this.markets.filter(market => market.storeName.toLowerCase().includes(searchValue.toLowerCase()) || market.description.toLowerCase().includes(searchValue.toLowerCase()))
-                : this.markets;
-        },
-        viewMarket(id) {
-            this.$router.push({ name: 'see-market', params: { id } });
-        },
+    viewMarket(id) {
+      this.$router.push({ name: 'see-market', params: { id } });
     },
+  }
 }
 </script>
 
 <style scoped>
 .row {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 1rem;
-    justify-items: center;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+  justify-items: center;
 }
 .col {
-    width: 100%;
+  width: 100%;
 }
 .market-image {
-    width: 27vw;
-    height: 18vw;
-    margin: 15px;
+  width: 27vw;
+  height: 18vw;
+  margin: 15px;
 }
 @media (max-width: 768px) {
-    .row {
-        grid-template-columns: repeat(2, 1fr);
-    }
+  .row {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 @media (max-width: 480px) {
-    .row {
-        grid-template-columns: 1fr;
-    }
+  .row {
+    grid-template-columns: 1fr;
+  }
 }
 </style>

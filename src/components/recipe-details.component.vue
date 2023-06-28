b944b8fec50fe2bdbcb9f811cc74ac9e4a311d79
Shayla Choque <89035883+ShaylaChoque@users.noreply.github.com>
2023-06-28 12:07:34 -0500

    recipe union with backend

--- src/components/recipe-details.component.vue
+++ src/components/recipe-details.component.vue
@@ -1,78 +1,102 @@
 <template>
+  <div>
     <ToolbarComponent/>
-  <div v-if="recipe" class="col">
-    <pv-card>
-      <template #title>
-        <pv-toolbar style="max-height: 65px; display: flex; align-items: center;">
-          <template #start>
-            <h1>{{ recipe.title }}</h1>
-          </template>
-          <template #end>
-            <router-link to="/mealthy/recipes">
-              <pv-button color="primary">Go Back</pv-button>
-            </router-link>
-          </template>
-        </pv-toolbar>
-        <pv-divider/>
-      </template>
-      <template #content>
-        <div class="card flex">
-          <div>
-            <p>{{ recipe.description }}</p>
-            <img :src="recipe.photoUrl" alt="Recipe Photo" class="recipe-image"  style="margin-top: 15px; width: 40vw;">
+    <div v-if="recipe" class="col">
+      <pv-card>
+        <template #title>
+          <pv-toolbar style="max-height: 65px; display: flex; align-items: center;">
+            <template #start>
+              <h1>{{ recipe.title }}</h1>
+            </template>
+            <template #end>
+              <router-link to="/mealthy/recipes">
+                <pv-button color="primary">Go Back</pv-button>
+              </router-link>
+            </template>
+          </pv-toolbar>
+          <pv-divider/>
+        </template>
+        <template #content>
+          <div class="card flex">
+            <div>
+              <p>{{ recipe.description }}</p>
+              <img :src="recipe.photoUrl" alt="Recipe Photo" class="recipe-image"  style="margin-top: 15px; width: 40vw;">
+            </div>
+            <pv-divider layout="vertical"/>
+            <div>
+              <h3>Ingredientes</h3>
+              <ul>
+                <li v-for="ingredient in ingredients" :key="ingredient.id">
+                  {{ ingredient.quantity }} {{ ingredient.name }} 
+                </li>
+              </ul>
+              <h3>Preparación</h3>
+              <ul>
+                <li v-for="step in steps" :key="step.id">
+                  {{ step.description }}
+                </li>
+              </ul>
+            </div>
           </div>
-          <pv-divider layout="vertical"/>
-          <div>
-            <h3>Ingredientes</h3>
-            <ul>
-              <li v-for="ingredient in recipe.ingredients" :key="ingredient.name">
-                {{ ingredient.quantity }} {{ ingredient.name }} 
-              </li>
-            </ul>
-            <h3>Preparación</h3>
-            <ul>
-              <li v-for="step in recipe.preparation" :key="step">
-                {{ step }}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </template>
-    </pv-card>
+        </template>
+      </pv-card>
+    </div>
   </div>
 </template>
-  
+
 <script>
 import { RecipeApiService } from '@/services/recipe-api.service';
 import ToolbarComponent from "@/components/toolbar.component.vue";
-  
+
 export default {
   name: 'RecipeDetailsComponent',
-    components: {ToolbarComponent},
+  components: { ToolbarComponent },
   data() {
     return {
       recipe: null,
+      ingredients: [],
+      steps: [],
       recipeService: new RecipeApiService()
     };
   },
   created() {
-    this.getRecipe();
+    this.getRecipeDetails();
   },
   methods: {
-    getRecipe() {
+    getRecipeDetails() {
       const recipeId = this.$route.params.id;
       this.recipeService.getById(recipeId)
         .then(response => {
           this.recipe = response.data;
+          this.getIngredientsByRecipeId(recipeId);
+          this.getStepsByRecipeId(recipeId);
         })
         .catch(error => {
           console.error('Error fetching recipe:', error);
         });
     },
+    getIngredientsByRecipeId(recipeId) {
+      this.recipeService.getIngredientByRecipeId(recipeId)
+        .then(response => {
+          this.ingredients = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching ingredients:', error);
+        });
+    },
+    getStepsByRecipeId(recipeId) {
+      this.recipeService.getStepByRecipeId(recipeId)
+        .then(response => {
+          this.steps = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching steps:', error);
+        });
+    },
   },
-}
+};
 </script>
   
 <style scoped>
 
-</style>  
+</style>
